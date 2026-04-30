2eb05e1c9822c9d3abf4ea395abfe8cccc0674a0
SNAPKITTYWEST <jessicalw34@gmail.com>
2026-04-30 03:24:20 -0600

    fix: extend session to 30 days

--- collectivekitty/pages/api/auth/[...nextauth].ts
+++ collectivekitty/pages/api/auth/[...nextauth].ts
@@ -10,6 +10,10 @@ export default NextAuth({
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
   callbacks: {
     async session({ session, token }) {
       return session
